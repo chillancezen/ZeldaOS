6d91fbe1434cb6242a46fc67e87d1025b77a7d1e
Link <jiezheng@vmware.com>
2018-08-19 02:23:19 -0400

    Add new APIs to READ/WRITE PCI config space

--- device/include/pci.h
+++ device/include/pci.h
@@ -37,9 +37,28 @@ struct pci_device {
 
     uint8_t irq_line;
     uint8_t irq_pin;
-
+    uint32_t bar[6];
+    uint32_t bar_size[6];
     struct pci_driver * driver;
 };
 void pci_init(void);
+
+uint16_t
+read_pci_config_space(uint8_t bus,
+    uint8_t device,
+    uint8_t function,
+    uint8_t offset);
+
+void
+write_pci_config_space(uint8_t bus,
+    uint8_t device,
+    uint8_t function,
+    uint8_t offset,
+    uint32_t data);
+
+uint32_t
+get_pci_bar(struct pci_device * _device, int32_t bar);
+uint32_t
+get_pci_bar_size(struct pci_device * _device, uint32_t bar);
 
 #endif
--- device/pci.c
+++ device/pci.c
@@ -33,7 +33,63 @@ read_pci_config_space(uint8_t bus,
     data = inl(PCI_CONFIG_DATA);
     return (data >> ((offset & 2) * 8)) & 0xffff;
 }
+void write_pci_config_space(uint8_t bus,
+    uint8_t device,
+    uint8_t function,
+    uint8_t offset,
+    uint32_t data)
+{
+    uint32_t addr = 0;
+    addr |= bus << 16;
+    addr |= (device & 0x0f) << 11;
+    addr |= (function & 0x7) << 8;
+    addr |= offset & 0xfc;
+    addr |= 0x80000000;
+    outl(PCI_OCNFIG_ADDRESS, addr);
+    outl(PCI_CONFIG_DATA, data);
+}
 
+uint32_t
+get_pci_bar(struct pci_device * _device, int32_t bar)
+{
+    uint8_t offset = 0x10 + bar * 4;
+    uint16_t hi;
+    uint16_t lo;
+    if(bar < 0 || bar >= 6)
+        return -1;
+    lo = read_pci_config_space(_device->bus,
+        _device->device,
+        _device->function,
+        offset);
+    hi = read_pci_config_space(_device->bus,
+        _device->device,
+        _device->function,
+        offset + 2);
+    return hi << 16 | lo; 
+}
+uint32_t
+get_pci_bar_size(struct pci_device * _device, uint32_t bar)
+{
+    uint8_t offset = 0x10 +bar * 4 ;
+    uint32_t bar_origin = get_pci_bar(_device, bar);
+    uint32_t size = 0;
+    if (bar < 0 || bar >= 6)
+        return -1;
+    write_pci_config_space(_device->bus,
+        _device->device,
+        _device->function,
+        offset,
+        0xffffffff);
+    size = get_pci_bar(_device, bar);
+    size = ~size;
+    size += 1;
+    write_pci_config_space(_device->bus,
+        _device->device,
+        _device->function,
+        offset,
+        bar_origin);
+    return size;
+}
 uint16_t
 get_pci_device_vendor_id(uint8_t bus,
     uint8_t device,
@@ -86,6 +142,7 @@ get_pci_device_interrupt(uint8_t bus,
 void
 enumerate_pci_devices(void)
 {
+    int bar;
     int bus;
     int device;
     int function;
@@ -120,6 +177,10 @@ enumerate_pci_devices(void)
                 _device->header_type = header_type;
                 _device->irq_line = interrupt & 0xff;
                 _device->irq_pin = (interrupt >> 8) & 0xff;
+                for(bar = 0; bar < 6; bar++) {
+                    _device->bar[bar] = get_pci_bar(_device, bar);
+                    _device->bar_size[bar] = get_pci_bar_size(_device, bar); 
+                }
                 list_append(&pci_device_list, &_device->list);
             }
         }
@@ -129,12 +190,14 @@ enumerate_pci_devices(void)
 void
 dump_pci_devices(void)
 {
+    uint8_t bar;
     struct list_elem * _list;
     struct pci_device * _device;
     LIST_FOREACH_START(&pci_device_list, _list) {
         _device = CONTAINER_OF(_list, struct pci_device, list);
         LOG_INFO("%x:%x.%x vendor_id:%x device_id:%x class:%x "
-            "subclass:%x header_type:%x irq_pin:%x irq_line:%x\n",
+            "subclass:%x\n       "
+            "header_type:%x irq_pin:%x irq_line:%x\n",
             _device->bus,
             _device->device,
             _device->function,
@@ -145,6 +208,14 @@ dump_pci_devices(void)
             _device->header_type,
             _device->irq_pin,
             _device->irq_line);
+        for(bar = 0; bar < 6; bar++) {
+            if (!_device->bar[bar])
+                continue;
+            printk("       bar %d: base:0x%x, size:0x%x\n",
+                bar,
+                _device->bar[bar],
+                _device->bar_size[bar]);
+        }
     }
     LIST_FOREACH_END();
 }
